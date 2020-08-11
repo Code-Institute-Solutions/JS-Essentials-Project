7498056523d41a4e26b4c592fbbb3274aecc4335
Matt Rudge <matt.rudge@codeinstitute.net>
2020-08-11 11:13:39 +0000

    maint: added tenth folder

--- 10-Updating The Scores/script.js
+++ 10-Updating The Scores/script.js
@@ -3,7 +3,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-    let gameType = "addition";
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
@@ -15,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
-    runGame(gameType);
+    runGame("addition");
 });
 
 // The main game "loop", called when the script is first loaded
@@ -43,7 +42,7 @@ function runGame(gameType) {
 
 function checkAnswer() {
 
-    let userAnswer = parseInt(document.getElementById("answer-box").value) || 0;
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateRightAnswer(); // calculatedAnswer is an array
     let isCorrect = userAnswer === calculatedAnswer[0]; // isCorrect has a true or false value
 
